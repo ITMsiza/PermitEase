5743ec935eb53366a6506595952e5d48bf68b4f6
Isaac Msiza <itmsiza1994@gmail.com>
2025-10-30 16:48:21 +0200

    Modified

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,8 @@
 import * as functions from "firebase-functions";
-import { https } from "firebase-functions";
 import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
-
-const app = next({
-  dev,
-  // Your Next.js build directory
-  conf: { distDir: "../.next" },
-});
-
+const app = next({ dev, conf: { distDir: "next" } });
 const handle = app.getRequestHandler();
 
 export const nextServer = functions.https.onRequest((req, res) => {
